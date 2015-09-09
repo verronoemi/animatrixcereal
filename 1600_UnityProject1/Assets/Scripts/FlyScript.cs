834c4094e7e8a8a121e7de197753976e28081bef
Veronica Martinez <martinez.veronicanoemi@gmail.com>
2015-09-09 16:08:26 -0600

    Conventions and Syntax

---
+++ 1600_UnityProject1/Assets/Scripts/FlyScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyScript : MonoBehaviour 
+{
+	private float heightTest = 3f;
+
+	void Start ()
+	{
+
+		Debug.Log (transform.position.x);
+
+		if(transform.position.x <= heightTest)
+
+		{
+			Debug.Log("I'm about to hit the ground!");
+			FlyAway();
+		}
+
+		else if(transform.position.x >= heightTest)
+
+		{
+			Debug.Log("I'm flying!");
+		}
+	}
+
+	private void FlyAway()
+
+	{
+		Debug.Log ("Fly Away!");
+		Vector3 newPosition = new Vector3(0,7,0);
+		transform.position = newPosition;
+	}
+}
