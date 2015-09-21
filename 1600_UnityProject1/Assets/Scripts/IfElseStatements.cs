4a29ea7bbfc14159f34c359b3b90171cb77eeae8
Veronica Martinez <martinez.veronicanoemi@gmail.com>
2015-09-21 15:45:44 -0600

    If Else Statements

---
+++ 1600_UnityProject1/Assets/Scripts/IfElseStatements.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfElseStatements : MonoBehaviour 
+{
+	public float coffeeTemperature = 85.0f;
+	public float hotLimitTemperature = 70.0f;
+	public float coldLimitTemperature = 40.0f;
+		
+		
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+			
+			coffeeTemperature -= Time.deltaTime * 5f;
+	}
+		
+		
+	void TemperatureTest ()
+	{
+		if (coffeeTemperature > hotLimitTemperature) 
+		{
+			print ("Too Hot");
+		} else if (coffeeTemperature < coldLimitTemperature) 
+		{
+			print ("Too Cold");
+		}
+		else 
+		{
+			print ("Just Right");
+		}
+	}
+}					      
