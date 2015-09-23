37c1b8b4fcc7fff3f5d412ecfe3299b262c0e1cf
Veronica Martinez <martinez.veronicanoemi@gmail.com>
2015-09-23 16:04:53 -0600

    Loops

---
+++ 1600_UnityProject1/Assets/Scripts/ForeachLoops.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForeachLoops : MonoBehaviour 
+{
+
+	void Start () 
+	{
+		string[] strings = new string[3];
+		
+		strings[0] = "first";
+		strings[1] = "second";
+		strings[2] = "third";
+		
+		foreach(string item in strings)
+		{
+			print (item);
+		}
+	}
+
+}
---
+++ 1600_UnityProject1/Assets/Scripts/WhileLoops.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoops : MonoBehaviour 
+{ //while loop
+	int cupsInTheSink = 4;
+
+	void Start ()
+	{
+		while (cupsInTheSink > 0) {
+			cupsInTheSink--;
+			Debug.Log ("I washed a cup");
+		}
+	}
+}
