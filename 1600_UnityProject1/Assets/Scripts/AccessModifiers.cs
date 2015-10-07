7e5d077a4c06ac26b6cb6a537ee35e453baa56da
Veronica Martinez <martinez.veronicanoemi@gmail.com>
2015-10-07 16:16:53 -0600

    Classes and Data Types

---
+++ 1600_UnityProject1/Assets/Scripts/AccessModifiers.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccessModifiers : MonoBehaviour 
+{
+	public int balloons = 5;
+	
+	private FlyScript otherClass;
+	
+	void Start () 
+	{
+		balloons = 35;
+		
+		//otherClass = new FlyScript();
+		//otherClass.FlyAway;
+	}
+}
