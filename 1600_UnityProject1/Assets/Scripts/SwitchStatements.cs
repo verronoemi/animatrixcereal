7fe25c9782303515137f8a4b3f937bc58edb5e5d
Veronica Martinez <martinez.veronicanoemi@gmail.com>
2015-11-02 16:16:21 -0700

    Switch Statements

---
+++ 1600_UnityProject1/Assets/Scripts/SwitchStatements.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchStatements : MonoBehaviour 
+{
+	public int score; 
+	
+	void Update()
+	{
+		switch(score)
+		{
+			case 5:
+				print ("Excellent");
+				break;
+			case 4:
+				print ("G0od job");
+				break;
+				
+				//... cases
+				
+			default:
+				print ("NOthing scored");
+				break;
+		}
+	}
+}
