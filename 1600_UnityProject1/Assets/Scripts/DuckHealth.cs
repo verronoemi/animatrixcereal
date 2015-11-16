f0cbf96d18dc9c725a40f964efeb0a6dc82b7375
Veronica Martinez <martinez.veronicanoemi@gmail.com>
2015-11-16 15:55:31 -0700

    For Dunt Hunt Sprites and GameObject Creation

---
+++ 1600_UnityProject1/Assets/Scripts/DuckHealth.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckHealth : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider hit)
+	{
+		if(hit.tag == "KillZone")
+		{
+			Destroy (this.gameObject);
+		}
+	}
+
+
+}
---
+++ 1600_UnityProject1/Assets/Scripts/DuckSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckSpawner : MonoBehaviour 
+{
+
+	public GameObject duck;
+	
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	public void SpawnDuck()
+	{
+		Debug.Log ("Spawn Duck");
+		//create the instantiate
+		Instantiate(duck, transform.position, Quaternion.identity); 
+	}
+}
